a370036affdc6aea541a44d72e4a9a2f1071a6c1
dit000 <dit000@dit000.local>
2019-03-13 15:35:14 +0900

    Initial Commit

---
+++ LeftRightButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeftRightButton
+//
+//  Created by dit000 on 2019. 3. 13..
+//  Copyright © 2019년 DIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
